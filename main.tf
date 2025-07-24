143d06b57e7bb413a094bc40d9c3796550cb2a59
Darshan-vitg <darshan.vaghasiya@vitg.us>
2025-07-24 12:20:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,174 +1,66 @@
 provider "aws" {
-  region = "us-east-1" # Change as needed
+  region = var.aws_region
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main-vpc"
-  }
+# Data sources for existing infrastructure
+data "aws_vpc" "existing" {
+  id = "vpc-0b75b0de8410ced50"
 }
 
-resource "aws_subnet" "subnet_a" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "subnet-a"
-  }
+data "aws_subnet" "subnet_1" {
+  id = "subnet-06b890f36c1d8aa84"
+}
+data "aws_subnet" "subnet_2" {
+  id = "subnet-0c512f1685f3cf34b"
+}
+data "aws_subnet" "subnet_3" {
+  id = "subnet-058ff11acd1ee121d"
+}
+data "aws_subnet" "subnet_4" {
+  id = "subnet-02f1c80839068cabe"
+}
+data "aws_subnet" "subnet_5" {
+  id = "subnet-0c6014d16d826cc10"
+}
+data "aws_subnet" "subnet_6" {
+  id = "subnet-0bb037e2c138aff42"
 }
 
-resource "aws_subnet" "subnet_b" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "subnet-b"
-  }
+data "aws_security_group" "existing" {
+  id = "sg-05e0b063a67841948"
 }
 
-resource "aws_subnet" "subnet_c" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-east-1c"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "subnet-c"
-  }
+data "aws_route_table" "existing" {
+  id = "rtb-0d75255b2973bdd63"
 }
 
-resource "aws_subnet" "subnet_d" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.4.0/24"
-  availability_zone       = "us-east-1d"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "subnet-d"
-  }
+data "aws_instance" "existing" {
+  instance_id = "i-09d300c017411c249"
 }
 
-resource "aws_subnet" "subnet_e" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.5.0/24"
-  availability_zone       = "us-east-1e"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "subnet-e"
-  }
-}
-
-resource "aws_subnet" "subnet_f" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.6.0/24"
-  availability_zone       = "us-east-1f"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "subnet-f"
-  }
-}
-
-resource "aws_security_group" "main_sg" {
-  name        = "main-sg"
-  description = "Allow all inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "main-sg"
-  }
-}
-
-resource "aws_route_table" "main_rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "main-rt"
-  }
-}
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.subnet_a.id
-  route_table_id = aws_route_table.main_rt.id
-}
-
-# Add similar route table associations for all other subnets...
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main-igw"
-  }
-}
-
-resource "aws_instance" "main_instance" {
-  ami                    = "ami-0c02fb55956c7d316" # Change based on region
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.subnet_a.id
-  vpc_security_group_ids = [aws_security_group.main_sg.id]
-  key_name               = "flask-key"
-
-  tags = {
-    Name = "main-instance"
-  }
-}
-
-resource "aws_iam_user" "dv_user" {
-  name = "DV"
-}
-
-resource "aws_iam_role" "support_role" {
+data "aws_iam_role" "support" {
   name = "AWSServiceRoleForSupport"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "support.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
 }
 
-resource "aws_iam_role" "trusted_advisor_role" {
+data "aws_iam_role" "trusted_advisor" {
   name = "AWSServiceRoleForTrustedAdvisor"
+}
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "trustedadvisor.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
+data "aws_iam_user" "dv" {
+  user_name = "DV"
+}
+
+# Example of restricted security group rule management (if you need to update it)
+resource "aws_security_group_rule" "ssh_ingress" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = data.aws_security_group.existing.id
+  cidr_blocks       = ["YOUR_WHITELISTED_IP/32"]
+  description       = "SSH access from trusted IP"
+}
+
+output "ec2_instance_public_ip" {
+  value = data.aws_instance.existing.public_ip
 }
